d4fd8dc7e60c5cbbdcb79a5388944a14bf136af8
Wiem-Themri <64691796+Wiem-Themri@users.noreply.github.com>
2020-07-11 17:18:48 +0200

    Updated home + footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
-  return (
-    <div className="container-fluid footer text-center ">
-      <h1 className="pt-3">Plantes & Jardins</h1>
-      <p>Nous contacter sur le 0773654088</p>
-      <p>3 rue andré derain 78280 Guyancourt</p>
-      <p>Nous rejoindre sur Facebook</p>
-      <p>Copyrights</p>
-      <p className="my-0 pb-3">
-        Created with love By Wiem Themri and Youssef Abdelmalek
-      </p>
-    </div>
-  );
+    return (
+        <div className="container-fluid footer text-center ">
+            <h1 className="pt-3">Plantes & Jardins</h1>
+            <p>Nous contacter sur (+33) 000000000</p>
+            {/* <p>3 rue andré derain 78280 Guyancourt</p> */}
+            <p>
+                Nous rejoindre sur{" "}
+                <Link to="https://www.facebook.com/plantes%20et%20jardins" className="fb-link">
+                    facebook
+                </Link>
+            </p>
+
+            <p>Copyrights 2020</p>
+            <p className="my-0 pb-3">
+                {/* Created with love By Wiem Themri and Youssef Abdelmalek */}
+            </p>
+        </div>
+    );
 }
