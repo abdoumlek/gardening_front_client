55d8cdd1dae33f4c7604620d377e9ce3ff9d4334
Wiem-Themri <64691796+Wiem-Themri@users.noreply.github.com>
2020-07-13 21:42:54 +0200

    update the Nav bar menu

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,55 +3,53 @@ import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 export default function Navigation() {
-  return (
-    <div className="w-100">
-      <div className="navbar navbar-expand-lg position-absolute w-100">
-        <Link to="/" className="navbar-brand">
-        Plantes & jardins
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapsibleNavbar"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse text-right" id="collapsibleNavbar">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Services
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Gallery
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/products" className="nav-link">
-                Products
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Contacts
-              </Link>
-            </li>
-          </ul>
+    return (
+        <div className="w-100">
+            <div className="navbar navbar-expand-lg position-absolute w-100">
+                <Link to="/" className="navbar-brand">
+                    Plantes & jardins
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#collapsibleNavbar"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    className="collapse navbar-collapse text-right"
+                    id="collapsibleNavbar"
+                >
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">
+                                Accueil
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/products" className="nav-link">
+                                Produits
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">
+                                Devis
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">
+                                Galerie
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">
+                                Contacts
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
