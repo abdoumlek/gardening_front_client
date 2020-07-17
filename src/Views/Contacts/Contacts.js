e91fea9f1dbb1e57fff3db76619cd883a732dc5f
Wiem-Themri <64691796+Wiem-Themri@users.noreply.github.com>
2020-07-17 14:25:33 +0200

    fixing contact page

--- src/Views/Contacts/Contacts.js
+++ src/Views/Contacts/Contacts.js
@@ -9,89 +9,125 @@ export default function Contacts() {
                 <h2 className="h2contactUs">
                     Avez-vous des questions ou des suggestions ?
                 </h2>
-                <p>Vous pouvez utiliser ce formulaire de contact :</p>
             </div>
-            <p className="mt-5">
-                Afin de répondre efficacement à votre demande, veuillez
-                sélectionner un sujet dans la liste suivante.
-            </p>
-
-            <form>
-                {" "}
-                <div className="contactForm mt-5">
-                    <div>
-                        <label>Objet de votre demande*</label>
-                        <div>
-                            <select required>
-                                <option disabled selected>
-                                    Sélectionnez un sujet
-                                </option>
-                                {/* <option value="1">Mon compte client</option> */}
-                                <option value="2">
-                                    Commander un produit non existant
-                                </option>
-                                <option value="3">
-                                    J'ai besoin d'un conseil
-                                </option>
-                                <option value="4">
-                                    Question concernat le délai de livraison
-                                    d'un produit
-                                </option>
-                                <option value="5">Autre</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div>
-                        <label>
-                            Nom* :
-                            <div>
-                                <input type="text" name="lastname" required />
-                            </div>
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            prénom* :
-                            <div>
-                                <input type="text" name="firstname" required />
-                            </div>
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            Email :
-                            <div>
-                                <input type="text" name="email" />
-                            </div>
-                        </label>
-                    </div>{" "}
-                    <div>
-                        <label>
-                            Numéro de téléphone* :
-                            <div>
-                                <input type="text" name="phone" required />
-                            </div>
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            Message* :
-                            <div>
-                                <textarea name="message" required></textarea>
-                            </div>
-                        </label>
-                    </div>
-                    <p>
-                        * Les champs marqués d'un astérisque (*) sont
-                        obligatoires
+            <div className=" container contactRowContainer">
+                <div className=" row col-12 p-5 d-flex justify-content-center align-items-center">
+                    <p className="mt-5">
+                        Afin de répondre efficacement à votre demande, veuillez
+                        sélectionner un sujet dans la liste suivante.
                     </p>
+                    <form>
+                        <div className="form-group mt-5">
+                            <div>
+                                <label>Objet de votre demande*</label>
+                                <div>
+                                    <select
+                                        className="form-control mb-4"
+                                        required
+                                    >
+                                        <option disabled selected>
+                                            Sélectionnez un sujet
+                                        </option>
+                                        {/* <option value="1">Mon compte client</option> */}
+                                        <option value="2">
+                                            Commander un produit non existant
+                                        </option>
+                                        <option value="3">
+                                            J'ai besoin d'un conseil
+                                        </option>
+                                        <option value="4">
+                                            Question concernat le délai de
+                                            livraison d'un produit
+                                        </option>
+                                        <option value="5">Autre</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label for="nom">Nom* :</label>
+
+                                <div>
+                                    <input
+                                        className="form-control"
+                                        type="text"
+                                        name="lastname"
+                                        id="nom"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label for="prenom">Prénom* :</label>
+
+                                <div>
+                                    <input
+                                        className="form-control"
+                                        type="text"
+                                        name="firstname"
+                                        id="prenom"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label for="email">Email :</label>
+
+                                <div>
+                                    <input
+                                        className="form-control"
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                    />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label for="message">
+                                    Numéro de téléphone* :
+                                </label>
+
+                                <div>
+                                    <input
+                                        className="form-control"
+                                        type="text"
+                                        name="phone"
+                                        id="message"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleFormControlTextarea1">
+                                    Message* :
+                                </label>
+
+                                <div>
+                                    <textarea
+                                        className="form-control"
+                                        name="message"
+                                        required
+                                        id="exampleFormControlTextarea1"
+                                        rows="6"
+                                    ></textarea>
+                                </div>
+                            </div>
+                            <p className="note mt-5">
+                                * Les champs marqués d'un astérisque (*) sont
+                                obligatoires
+                            </p>
+                        </div>
+
+                        <div className="text-center mt-5">
+                            <button
+                                className="btn  btnSubmitForm mb-5"
+                                type="submit"
+                            >
+                                Envoyer mon message
+                            </button>
+                        </div>
+                    </form>
                 </div>
-                <div className="text-center">
-                    <button className="btn  btnSubmitForm mb-5" type="submit">
-                        Envoyer mon message
-                    </button>
-                </div>
-            </form>
+            </div>
         </div>
     );
 }
